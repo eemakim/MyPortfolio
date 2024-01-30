82f07dffb421af24206d1e77e76039cc0119c8a8
Emma Kim <eema.kim01@gmail.com>
2024-01-30 13:12:20 +0100

    updating styling for portfolio section

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ fetch('projects.json').then(function (response) {
     console.log(data.projects)
 
     data.projects.forEach(function (project) {
-        projectsDisplay.innerHTML += `<div>
-        ${project.img}
-        <h4>${project.projectName}</h4>
-        <p>${project.projectDesc}</p>
-        <a href="${project.githubLink}">See more on Github</a>
+        projectsDisplay.innerHTML += `<div class="project-items">
+        <div>${project.img}</div>
+        <p class="project-item desc">${project.projectDesc}</p>
+        <div class="link-buttons"><a href="${project.githubLink}" target="_blank"><h4><span id="code">< </span>${project.projectName}<span> /></span></h4></a></div>
         </div>`
     })
 })
