bde39a90a75f3f6627ba6699173212ba31997d4a
Emma Kim <eema.kim01@gmail.com>
2024-01-30 18:54:39 +0100

    add js to contact form to prevent submit if input fields blank

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ fetch('projects.json').then(function (response) {
         <div>${project.img}</div>
         <p class="project-item desc">${project.projectDesc}</p>
         <div class="link-buttons"><a href="${project.githubLink}" target="_blank"><h4><span id="code">< </span>${project.projectName}<span> /></span></h4></a></div>
-        </div>`
+        </div><span>hello</span>`
     })
+})
+
+
+// *** Form submit event ***
+
+
+const submitMessage = document.querySelector('.form')
+const nameInput = document.querySelector('#name').value
+const emailInput = document.querySelector('#email').value
+const textInput = document.querySelector('#message').value
+
+submitMessage.addEventListener('submit', function (e) {
+
+    if (
+        (nameInput.length === 0) ||
+        (emailInput.length === 0) ||
+        (textInput.length < 15)
+    ) {
+        console.log('form is submitting')
+        e.preventDefault()
+
+        // FOR WEDNESDAY 31st JAN
+        // form doesnt submit even if all fields are filled????
+
+    }
 })
